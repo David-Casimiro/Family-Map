457790a2076cff22ee74cff40dc307fc4ef7b7ae
David Casimiro <73147712+David-Casimiro@users.noreply.github.com>
2026-06-22 21:51:46 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,8 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  
-    const firebaseConfig = {
-    apiKey: "AIzaSyBnmUDtg2Le-9YzU9YiR09p1y8BKVNTq5g",
-    authDomain: "family-map-bf146.firebaseapp.com",
-    projectId: "family-map-bf146",
-    storageBucket: "family-map-bf146.firebasestorage.app",
-    messagingSenderId: "357243545790",
-    appId: "1:357243545790:web:479c80fbc1233c4ddcd6ef",
-    measurementId: "G-WR1C10VQFB"
-  };
-
-   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
